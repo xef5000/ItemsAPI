658d6f020721a44e0ed79c1037caa560c0118ccb
xef5000 <rom.deshaies@gmail.com>
2025-07-12 17:23:50 -0400

    Bump version to 1.1.1 in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 allprojects {
     // Set the group and version for all modules. Replaces <groupId> and <version>.
     group = "com.github.xef5000"
-    version = "1.1.0c"
+    version = "1.1.1"
 
     // Define the repositories for all modules. Replaces the <repositories> section.
     // By defining them here, you don't need to repeat them in every child module.
