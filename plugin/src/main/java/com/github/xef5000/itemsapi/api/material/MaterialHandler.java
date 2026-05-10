9a959527872941d7e34ab6f0d8290252af1e1db4
xef5000 <rom.deshaies@gmail.com>
2026-05-09 21:04:26 -0400

    Remove deprecated parse method for ConfigurationSection in MaterialHandler

--- plugin/src/main/java/com/github/xef5000/itemsapi/api/material/MaterialHandler.java
+++ plugin/src/main/java/com/github/xef5000/itemsapi/api/material/MaterialHandler.java
@@ -22,18 +22,4 @@ public interface MaterialHandler {
      * Will receive "custom_data", not "prefix:custom_data".
      * */
     ItemStack parse(String materialString);
-
-    /**
-     * Parses a ConfigurationSection to create an ItemStack.
-     * This method expects the section to have a "material" field
-     */
-    default ItemStack parse(ConfigurationSection section) {
-        String data = getPrefix().isEmpty() ?
-                section.getString("material") :
-                section.getString("material").substring(getPrefix().length() + 1);
-        if (data == null || data.isEmpty()) {
-            throw new IllegalArgumentException("Material string cannot be null or empty.");
-        }
-        return parse(data);
-    }
 }
