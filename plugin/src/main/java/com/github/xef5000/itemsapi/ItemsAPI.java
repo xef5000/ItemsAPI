36f552c354d081dd222f985de171c72e0abfcad6
xef5000 <rom.deshaies@gmail.com>
2026-05-09 21:18:17 -0400

    Refactor ItemBuilder to accept material string directly and consolidate material handler registration

--- plugin/src/main/java/com/github/xef5000/itemsapi/ItemsAPI.java
+++ plugin/src/main/java/com/github/xef5000/itemsapi/ItemsAPI.java
@@ -1,68 +1,20 @@
 package com.github.xef5000.itemsapi;
 
 import com.github.xef5000.itemsapi.api.ItemBuilder;
-import com.github.xef5000.itemsapi.api.material.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class ItemsAPI {
 
-    // A list of all registered material handlers
-    private static final List<MaterialHandler> materialHandlers = new ArrayList<>();
-
-    // Static initializer to register your handlers
-    static {
-        // Custom plugins material handlers
-        registerHandler(new NexoMaterialHandler());
-        registerHandler(new ItemsAdderMaterialHandler());
-        registerHandler(new OraxenMaterialHandler());
-        registerHandler(new MMOItemsMaterialHandler());
-        registerHandler(new HeadDatabaseMaterialHandler());
-        registerHandler(new MythicMobsMaterialHandler());
-        registerHandler(new ExecutableItemsMaterialHandler());
-        registerHandler(new QualityArmoryMaterialHandler());
-        registerHandler(new NovaMaterialHandler());
-        registerHandler(new EcoMaterialHandler());
-        registerHandler(new WildToolsMaterialHandler());
-
-        // Minecraft mechanic handlers
-        registerHandler(new Base64MaterialHandler());
-
-        registerHandler(new MinecraftMaterialHandler()); // Fallback
-    }
-
-    public static void registerHandler(MaterialHandler handler) {
-        // Prepend to ensure custom handlers are checked before the default
-        materialHandlers.add(handler);
-    }
-
     public static ItemBuilder createBuilder(String materialString, int amount, String name, int customModelData, List<String> lore, int durability, boolean unbreakable, Map<String, Object> components, Map<Enchantment, Integer> enchantments, List<String> flags) {
-        ItemStack item = null;
-        for (MaterialHandler handler : materialHandlers) {
-            if (handler.canHandle(materialString)) {
-                String data = handler.getPrefix().isEmpty() ?
-                        materialString :
-                        materialString.substring(handler.getPrefix().length() + 1);
-                if (data.isEmpty())
-                    throw new IllegalArgumentException("Material string cannot be null or empty.");
-
-                item = handler.parse(materialString);
-                break;
-            }
-        }
-
-        if (item == null) {
-            throw new IllegalArgumentException("Could not handle material string: " + materialString);
-        }
-
         // Apply all other optional properties
-        return new ItemBuilder(item,
+        return new ItemBuilder(
+                materialString,
                 amount,
                 name,
                 customModelData,
--- plugin/src/main/java/com/github/xef5000/itemsapi/api/ItemBuilder.java
+++ plugin/src/main/java/com/github/xef5000/itemsapi/api/ItemBuilder.java
@@ -1,6 +1,7 @@
 package com.github.xef5000.itemsapi.api;
 
 import com.github.xef5000.itemsapi.api.adapter.ItemMetaAdapter;
+import com.github.xef5000.itemsapi.api.material.*;
 import com.github.xef5000.itemsapi.api.utils.ColorHelper;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
@@ -9,6 +10,7 @@ import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -22,6 +24,7 @@ public class ItemBuilder {
     private final NMSAdapter nmsAdapter = VersionFactory.getNmsAdapter(); // Get NMS adapter
 
     // Values
+    public final String material;
     public final int amount;
     public final String name;
     public final Integer customModelData;
@@ -32,10 +35,11 @@ public class ItemBuilder {
     public final Map<Enchantment, Integer> enchantments;
     public final List<String> flags;
 
-    public ItemBuilder(ItemStack item, int amount, String name, int customModelData, List<String> lore, int durability, boolean unbreakable, Map<String, Object> components, Map<Enchantment, Integer> enchantments, List<String> flags) {
-        this.item = item;
+    public ItemBuilder(String material, int amount, String name, int customModelData, List<String> lore, int durability, boolean unbreakable, Map<String, Object> components, Map<Enchantment, Integer> enchantments, List<String> flags) {
+        this.item = parseItemFromName(material);
         this.meta = item.getItemMeta();
 
+        this.material = material;
         this.amount = amount;
         this.name = name;
         this.customModelData = customModelData;
@@ -131,6 +135,56 @@ public class ItemBuilder {
 
     public ItemStack apply() {
         item.setItemMeta(meta);
+        return item;
+    }
+
+    // A list of all registered material handlers
+    private static final List<MaterialHandler> materialHandlers = new ArrayList<>();
+
+    // Static initializer to register your handlers
+    static {
+        // Custom plugins material handlers
+        registerHandler(new NexoMaterialHandler());
+        registerHandler(new ItemsAdderMaterialHandler());
+        registerHandler(new OraxenMaterialHandler());
+        registerHandler(new MMOItemsMaterialHandler());
+        registerHandler(new HeadDatabaseMaterialHandler());
+        registerHandler(new MythicMobsMaterialHandler());
+        registerHandler(new ExecutableItemsMaterialHandler());
+        registerHandler(new QualityArmoryMaterialHandler());
+        registerHandler(new NovaMaterialHandler());
+        registerHandler(new EcoMaterialHandler());
+        registerHandler(new WildToolsMaterialHandler());
+
+        // Minecraft mechanic handlers
+        registerHandler(new Base64MaterialHandler());
+
+        registerHandler(new MinecraftMaterialHandler()); // Fallback
+    }
+
+    public static void registerHandler(MaterialHandler handler) {
+        // Prepend to ensure custom handlers are checked before the default
+        materialHandlers.add(handler);
+    }
+
+    public static ItemStack parseItemFromName(String materialString) {
+        ItemStack item = null;
+        for (MaterialHandler handler : materialHandlers) {
+            if (handler.canHandle(materialString)) {
+                String data = handler.getPrefix().isEmpty() ?
+                        materialString :
+                        materialString.substring(handler.getPrefix().length() + 1);
+                if (data.isEmpty())
+                    throw new IllegalArgumentException("Material string cannot be null or empty.");
+
+                item = handler.parse(materialString);
+                break;
+            }
+        }
+
+        if (item == null)
+            throw new IllegalArgumentException("Could not handle material string: " + materialString);
+
         return item;
     }
 }
